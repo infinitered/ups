f3614a69f432df8e9ce35ee376750f186ef11105
Zachary Berkompas <zberkom@gmail.com>
2017-07-05 11:59:06 -0700

    Initial Commit

---
+++ lib/ups.ex
@@ -0,0 +1,18 @@
+defmodule Ups do
+  @moduledoc """
+  Documentation for Ups.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ups.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ups_test.exs
@@ -0,0 +1,8 @@
+defmodule UpsTest do
+  use ExUnit.Case
+  doctest Ups
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
